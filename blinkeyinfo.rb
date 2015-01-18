bbcb6cfc30fc9ed638503b21048950459792ca9b
Tristan Hume <tris.hume@gmail.com>
2015-01-18 17:25:58 -0500

    Scheduling and fanciness

--- blinkeyinfo.rb
+++ blinkeyinfo.rb
@@ -1,12 +1,24 @@
 #!/usr/bin/env ruby
 
-# require "blink1"
+require "blink1"
 
 def set_colour(r,g,b)
-  # Blink1.open do |blink1|
-  #   blink1.fade_to_rgb(r,g,b)
-  # end
-  system "blink1-tool -q --rgb=#{r.to_i},#{g.to_i},#{b.to_i}"
+  Blink1.open do |blink1|
+    blink1.fade_to_rgb(100,r,g,b)
+  end
+  # system "blink1-tool -q --rgb=#{r.to_i},#{g.to_i},#{b.to_i}"
 end
 
-set_colour(200,0,200)
+def load_average
+  res = `sysctl vm.loadavg`.split
+  res[2].to_f
+end
+
+case
+when Time.now.hour >= 22 || Time.now.hour < 7
+  set_colour(0,0,0)
+when load_average > 2.0
+  set_colour(200,0,0)
+else
+  set_colour(0,120,200)
+end
